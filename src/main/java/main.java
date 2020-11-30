c511d26a001b2c70aff688a23e72d4c0bf61ce27
Parker Douglas <54992963+DragonTamrr@users.noreply.github.com>
2020-11-30 11:05:32 -0600

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,7 +6,7 @@ import javax.security.auth.login.LoginException;
 
 public class main {
     public static void main(String[] args) throws LoginException {
-        JDABuilder jdabuilder = new JDABuilder("Nzc1NDU2NjM1MjcwODU2NzY2.X6mmRQ.CG8tdSOFTNMWaKk_e5N06rXFvu4")
+        JDABuilder jdabuilder = new JDABuilder("Insert Token Here")
                 .setActivity(Activity.playing("type \"!fisd help\", if you have questions!"));
         JDA jda = jdabuilder.build();
 
